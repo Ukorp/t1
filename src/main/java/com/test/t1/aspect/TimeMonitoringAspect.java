e28f33fb5158ce1ac019235ed0d12d3cba54b5e2
ukorp <ukorp63@gmail.com>
2025-03-27 20:52:59 +0300

    fix: внесены правки

--- src/main/java/com/test/t1/aspect/TimeMonitoringAspect.java
+++ src/main/java/com/test/t1/aspect/TimeMonitoringAspect.java
@@ -1,7 +1,6 @@
 package com.test.t1.aspect;
 
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
